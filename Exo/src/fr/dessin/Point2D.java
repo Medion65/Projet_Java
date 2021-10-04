244a42c2758d5a9527cb1405b29f2936cb067917
David Saniez <david.saniez@gmail.com>
2021-10-04 14:14:17 +0200

    Add empty constructor & fill constructor

--- Exo/src/fr/dessin/Point2D.java
+++ Exo/src/fr/dessin/Point2D.java
@@ -4,6 +4,16 @@ public class Point2D {
     private Integer x;
     private Integer y;
 
+    public Point2D() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point2D(Integer vX, Integer vY) {
+        this.x = vX;
+        this.y = vY;
+    }
+
     public Integer getX() {
         return x;
     }
